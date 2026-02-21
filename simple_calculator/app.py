47d1da7d5f8c1a7773665270dc5442ad0e81a7bf
mdqais29 <qaismd29@gmail.com>
2026-02-21 15:43:36 +0530

    added simple calculator

---
+++ simple_calculator/app.py
@@ -0,0 +1,26 @@
+print("***Simple Calculator***")
+
+num1 = float(input("Enter the First Number:"))
+num2 = float(input("Enter the Second Number:"))
+
+print("Choose the Operation:")
+print("a) Add")
+print("b) Subtract")
+print("c) Multipy")
+print("d) Divide")
+
+choice = input("Enter your choice (a,b,c,d):")
+
+if choice == "a":
+    print("Result = ", num1 + num2)
+elif choice == "b":
+    print("Result = ", num1 - num2)
+elif choice == "c":
+    print("Result = ", num1 * num2)
+elif choice == "d":
+    if num2 != 0:
+        print("Result = ", num1 / num2)
+    else:
+        print("Can't Divide by 0")
+else:
+    print("Invalid Choice!")
