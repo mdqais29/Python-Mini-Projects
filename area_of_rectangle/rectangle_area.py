7bc800a89acf86e89389bcb7ae5b2553330c6a06
mdqais29 <qaismd29@gmail.com>
2026-02-24 15:00:49 +0530

    added program to find area of rectangle

---
+++ area_of_rectangle/rectangle_area.py
@@ -0,0 +1,8 @@
+#program to print the area of the rectangle.
+
+l = int(input("Enter the Length of a rectangle:"))
+b = int(input("Enter the Breadth of a rectangle:"))
+
+area = l*b
+
+print(f"The area of Rectangle is {area}")
